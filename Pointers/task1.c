eec8a6f74da70c017c3f8326894cc15babe24f88
Xander <vlad.dyadov@mail.ru>
2020-08-03 22:26:53 +0700

    Указатели

--- Pointers/task1.c
+++ Pointers/task1.c
@@ -5,11 +5,13 @@ int main (void){
 	unsigned int a = 0xAABBCCDD;
 	unsigned char *ptr;
 	ptr = (unsigned char *) &a;
+	
+	ptr += 3;	
 
-	for (int i = 4; i > 0; --i){
+	for (int i = 1; i <= 4; ++i){
 
 		printf("%d byte of a = 0x%X\n", i, *ptr);
-		ptr++;
+		ptr--;
 	}
 
 	return 0;
